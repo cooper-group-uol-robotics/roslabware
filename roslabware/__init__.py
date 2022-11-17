b44faf0295099c3a85e4b5e5e305f0616b54f381
smanzanoec@gmail.com <smanzanoec@gmail.com>
2022-11-17 17:35:58 +0000

    add init files

--- roslabware/__init__.py
+++ roslabware/__init__.py
@@ -1,11 +1,10 @@
 # IKA
-from .devices.ika_ret_control_visc import RETControlViscHotplateRos
-
 # Fisher Balance
 from .devices.fisher_balance_pp14102 import BalancePPS4102Ros
-
-# Mettler Quantos
-from .devices.mettler_quantos_qb1 import QuantosQB1Ros
+from .devices.ika_ret_control_visc import RETControlViscHotplateRos
 
 # Balance
 from .devices.kern_pcb_top_balance import TopBalanceRos
+
+# Mettler Quantos
+from .devices.mettler_quantos_qb1 import QuantosQB1Ros
